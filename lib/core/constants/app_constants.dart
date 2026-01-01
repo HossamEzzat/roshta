fbfb6a0e7ccfa07dd12fa4816fc49e475788e99e
Hossam Ezzat <77586282+HossamEzzat@users.noreply.github.com>
2026-01-01 14:03:02 +0200

    Update app_constants.dart

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,5 +1,5 @@
 class AppConstants {
   static const String appName = 'Roshta';
   // TODO: Replace with your actual Gemini API Key
-  static const String geminiApiKey = 'AIzaSyCB17pKt-ttIueyaiRu-lHEv-h0WSGUVYw';
+  static const String geminiApiKey = '';
 }
